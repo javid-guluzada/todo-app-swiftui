6868cdbe4f2f0004a04925e8bdf41606e535853d
Cavid Quluzadə <cqthor@gmail.com>
2024-08-18 15:32:45 +0400

    dark mode and rsponsive

--- TodoListApp/TodoListAppApp.swift
+++ TodoListApp/TodoListAppApp.swift
@@ -17,6 +17,7 @@ struct TodoListAppApp: App {
             NavigationView{
                 ListView()
             }
+            .navigationViewStyle(StackNavigationViewStyle())
             .environmentObject(listViewModel)
         }
     }
